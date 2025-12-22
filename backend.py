f6a1ba202635cdb0b8bb108c88fa937c9a99f8e3
Ksiezyc7 <github@programcode.pl>
2025-12-22 18:47:29 +0100

    added mc version and modloader flags, improved data loading, improved data output

--- backend.py
+++ backend.py
@@ -5,15 +5,27 @@ api_url = "https://api.modrinth.com/v2"
 #api_url = "http://localhost:8000"
 user_agent = "Ksiezyc7/mod-download-util"
 
-def search_mod(query, limit, mc_ver):
-    res = requests.request("GET", f"{api_url}/search", params={"query" : query, "limit" : limit, "facets" : f"[[\"project_type=mod\"],[\"versions:{mc_ver}\"]]", }, headers={"User-Agent" : user_agent})
+def search_mod(query, limit, mc_ver, loader):
+    if(mc_ver != "*"):
+        res = requests.request("GET", f"{api_url}/search", params={"query" : query, "limit" : limit, "facets" : f"[[\"project_type=mod\"],[\"versions:{mc_ver}\"],[\"categories:{loader}\"]]", }, headers={"User-Agent" : user_agent})
+    else:
+        res = requests.request("GET", f"{api_url}/search", params={"query" : query, "limit" : limit, "facets" : f"[[\"project_type=mod\"],[\"categories:{loader}\"]]", }, headers={"User-Agent" : user_agent})
+
+    
+    
     if(res.status_code != 200):
         return None
     json_data = res.json()
     if("hits" not in json_data or len(json_data["hits"]) <= 0):
         return None
     return json_data["hits"]
-    
+def get_newest_version_that_supports_minecraft_v(project_id: str, mcv: str, loader: str):
+    res = requests.request("GET", f"{api_url}/project/{project_id}/version", params={"loaders" : [loader], "game_versions" : [mcv]}, headers={"User-Agent" : user_agent})
+    if(res.status_code != 200):
+        return None
+    json_data = res.json()
+    pass
+
 def get_dependencies(project_id):
     res = requests.request("GET", f"{api_url}/project/{project_id}/dependencies", headers={"User-Agent" : user_agent})
 
--- modutil.py
+++ modutil.py
@@ -2,18 +2,28 @@ import requests
 import defs
 from backend import *
 from args_parser import *
+import re
+
+
 add_flag("-s") # Search
+add_flag("-g") # Get version id
 add_flag("-f") # Find dependencies
 add_flag("-d") # Download
 add_flag("-o", 1) # output file, defaults to "mods.txt"
 add_flag("-c", 1) # amount of search hits to show
+add_flag("-v", 1) # minecraft version
+add_flag("-l", 1) # minecraft loader
+
 ofilename = "mods.txt"
 ifilename = ""
 top_c = 5 # amount of hits to show, defaults to 5
-mc_loader = 0
+mc_loader = None
 m_version = "1.21.11" # minecraft version to search for, defaults to newest, as of 17.12.2025 it's 1.21.11
 
 args = parse_args()
+
+# Set variables based on provided flags (its so fucking ugly :sob: )
+
 if(len(args["_"]) > 0):
     ifilename = args["_"][0]
 if("-o" in args):
@@ -23,9 +33,19 @@ if("-c" in args):
         top_c = 5
     else:
         top_c = int(args["-c"][0]) if args["-c"][0].isnumeric() else 5
-print(args)
+
+if("-v" in args):
+    if(args["-v"][0] != None):
+        m_version = args["-v"][0]
+
+if("-l" in args):
+    if(args["-l"][0] != None):
+        mc_loader = args["-l"][0]
+
+# What actions to do
 
 search = "-s" in args.keys()
+get_version = "-g" in args.keys()
 find_dependecies = "-f" in args.keys()
 download = "-d" in args.keys()
 
@@ -36,16 +56,54 @@ if(not (search or find_dependecies or download)):
     print("use -h to see actions\n")
     exit()
 
+# Validate some provided data
+
+known_loaders = ["fabric", "forge", "quilt", "neoforge"]
+
+if(mc_loader not in known_loaders):
+    print(f"Unknown mod loader {mc_loader}")
+    print("Do you wish to continue? y/N")
+    choice = input("> ")
+    if(len(choice) > 0 and choice[0].lower() == "y"):
+        pass
+    else:
+        exit()
+    print("")
+
+# Checks if Minecraft version matches regex
+# Either X.X.X or X.X (X can be any number)
+# If version is set to * (any version) this check is skipped
+if(re.fullmatch(r"[0-9]+\.[0-9]+(\.[0-9]+)?", m_version) == None and m_version != "*"):
+    print(f"Invalid? Minecraft version {m_version}")
+    print("Do you wish to continue? y/N")
+    choice = input("> ")
+    if(len(choice) > 0 and choice[0].lower() == "y"):
+        pass
+    else:
+        exit()
+    print("")
 
 
 
-input_file_type = 0 # 0 for mod name list, 1 for id list
+
 operating_data = []
+
+def load_data():
+    try:
+        f = open(ifilename, "r")
+    except FileNotFoundError:
+        print(f"Could not open file {ifilename}")
+        exit(1)
+    for line in f.readlines():
+        if("::" in line):
+            operating_data.append(line.strip("\n\r").split("::", 1))
+        else:
+            operating_data.append(line.strip("\n\r"))
 
 def search_multiple(queries: list):
     selected_mods = []
     for q in queries:
-        hits = search_mod(q, top_c, m_version)
+        hits = search_mod(q, top_c, m_version, mc_loader)
         if(hits == None):
             continue
         if(top_c == 1):
@@ -60,36 +118,14 @@ def search_multiple(queries: list):
             hit_number += 1
         selected = input("> ")
         print("")
+        if(selected.lower() == "x"):
+            print("Action aborted by user")
+            exit()
         if(selected.isdigit() and int(selected) > 0 and int(selected) <= len(hits)):
             selected_mods.append(hits[int(selected) - 1]["project_id"])
     return selected_mods
 
 
-def load_data():
-    if(search):
-        try:
-            f = open(ifilename, "r")
-        except FileNotFoundError:
-            print(f"Could not open file {ifilename}")
-            exit(1)
-        for line in f.readlines():
-            operating_data.append(line.lstrip("\n").lstrip("\r"))
-    elif(find_dependecies):
-        try:
-            f = open(ifilename, "r")
-        except FileNotFoundError:
-            print(f"Could not open file {ifilename}")
-            exit(1)
-        for line in f.readlines():
-            operating_data.append(line.lstrip("\n").lstrip("\r"))
-    elif(download):
-        try:
-            f = open(ifilename, "r")
-        except FileNotFoundError:
-            print(f"Could not open file {ifilename}")
-            exit(1)
-        for line in f.readlines():
-            operating_data.append(line.lstrip("\n").lstrip("\r").split("::", 1))
 
 
 
@@ -98,7 +134,16 @@ def load_data():
 
 def do_search_action():
     global operating_data
+    if(mc_loader == None):
+        print("Mod loader not specified!")
+        exit(2)
     operating_data = search_multiple(operating_data)
+
+def do_get_version_action():
+    global operating_data
+
+    pass
+
 
 def do_find_dependencies_action():
     global operating_data
@@ -112,8 +157,12 @@ if(ifilename == ""):
     print("No input file")
     exit(1)
 
-
-
+of = None
+def out_text(t):
+    if(of == None):
+        print(t, end="")
+    else:
+        of.write(t)
 
 
 
@@ -125,23 +174,20 @@ if(ifilename == ""):
 
 
 def output():
-    if(ofilename == None):
-        for l in operating_data:
-            if(type(l) == list):
-                print(f"{l[0]}::{l[1]}")
-            else:
-                print(l)
-    else:
+    global of
+    if(ofilename != None):
         try:
-            f = open(ofilename, "w")
-        except:
+            of = open(ofilename, "w")
+        except: 
             print(f"Could not open file {ofilename}")
-        for l in operating_data:
-            if(type(l) == list):
-                f.write(f"{l[0]}::{l[1]}\n")
-            else:
-                f.write(f"{l}\n")
-        f.close()
+            exit(1)
+    for l in operating_data:
+        if(type(l) == list and len(l) >= 2):
+            out_text(f"{l[0]}::{l[1]}\n")
+        else:
+            out_text(f"{l}\n")
+
+
 
 
 if __name__ == "__main__":
